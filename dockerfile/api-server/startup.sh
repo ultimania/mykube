26f441e298f054b951dd7837d3bdbe0d3725d478
Your Name <you@example.com>
2019-07-30 10:27:21 +0900

    add twitter api pod

--- dockerfile/api-server/startup.sh
+++ dockerfile/api-server/startup.sh
@@ -1,2 +1,3 @@
 #/bin/sh
-python /root/${PROJECT_NAME}/manage.py runserver 0.0.0.0:8000
+git clone -b twitter_api https://github.com/ultimania/investmentTools.git
+python /root/investmentTools/manage.py runserver 0.0.0.0:8000
